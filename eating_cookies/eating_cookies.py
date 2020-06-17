dc0b2eeb44de84fc9d5001fad4dacc9088be6b6a
RobertMisch <robertmisch94@gmail.com>
2020-06-17 18:34:27 -0400

    2/5 done, gonna be a long night

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,35 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    #so I'm going to go about this in a Tri-nary fashion
+    #initialize the bytes 
+    register=[]
+    total=1
+    for i in range(n):
+        register.append(1)
 
+def permintations():
     pass
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
+    #draw out what the answer should be
+    """
+    total permintations
+    1,1,1,1,1
+    1,1,1,2
+    1,1,2,1
+    1,2,1,1
+    2,1,1,1
+    2,1,2
+    2,2,1
+    1,1,3
+    1,3,1
+    3,1,1
+    2,3
+    3,2
+    """
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,71 @@
+def count_sort(arr, maximum=None):
+    # Your code here
+    #we need to build an array for each number up to our maximum
+    if len(arr)==0:
+        return arr
+    if maximum==None:
+        maximum=0
+        for number in arr:
+            if number > maximum:
+                maximum=number
+    count_arr=[] 
+    sorted_arr=[]
+    for i in range(len(arr)):
+        sorted_arr.append(0)
+    for i in range(maximum+1):
+        count_arr.append(0)
+    for number in arr:
+        if number >=0:
+            count_arr[number]+=1
+        else:
+            # print("Error, negative numbers not allowed in Count Sort")
+            return "Error, negative numbers not allowed in Count Sort"
+    for index in range(1, len(count_arr)):
+        count_arr[index] = count_arr[index] + count_arr[index-1]
+    #so we need to look at our original array value
+    #we check the count_arr[og value]
+    #whatever number is stored in that index, is the correct placement of the og value
+    #then we subtract 1 from the count on the count_arr[og value]
+    #then we move to the next index in our og array
+
+    for number in arr:
+        sorted_arr[count_arr[number]-1] = number
+        count_arr[number]-=1
+
+    return sorted_arr
+
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # so we need to take a list, and find a single value in that list
+    #we could create a flag system, similar to count sort and the primes algorithm
+    #we'll find the maximum value in the list
+    #these flags will indicate which values have duplicates
 
-    pass
+    #finding the max value
+    max_value=0
+    for number in arr:
+        if number > max_value:
+            max_value=number
+    
+    #flag system initialization
+    flags=[]
+    for i in range(max_value+1):
+        flags.append(0)
+    #then we need to mark the values as counted. the value is the index on flags
+    for number in arr:
+        flags[number]+=1
+    #search through flags to see what has only 1 entry
+    for index, flag in enumerate(flags):
+        if flag == 1:
+            return index
+
+    
+    
+
 
 
 if __name__ == '__main__':
