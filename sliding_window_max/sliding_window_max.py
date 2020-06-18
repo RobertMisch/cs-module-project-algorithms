83d2f26159d272d68d07b3f8a809ce451c1365a5
RobertMisch <robertmisch94@gmail.com>
2020-06-17 23:29:36 -0400

    4/5 done

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,11 +1,24 @@
+import collections
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    # my_queue= collections.deque()
+    answer=[]
+    for i in range(len(nums)-(k-1)):
+        slide=nums[i:i+(k)]
+        # print(slide)
+        window_max=nums[i]
+        for num in slide:
+            if num > window_max:
+                window_max=num
+        answer.append(window_max)
+    return answer
+            
 
-    pass
+
 
 
 if __name__ == '__main__':
