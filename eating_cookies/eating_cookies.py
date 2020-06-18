f0bb4991b9ad96fbb01155032194184704a1bd6b
RobertMisch <robertmisch94@gmail.com>
2020-06-18 01:16:33 -0400

    Basically done. just some finished touches needed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,15 +3,31 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    #so I'm going to go about this in a Tri-nary fashion
-    #initialize the bytes 
-    register=[]
-    total=1
+    # if(n<=2):
+    #     return 0
+    # elif(n==3):
+    #     return 1
+    # else:
+    #     total=(eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
+    #     return total
+    myArr=[1]
+    answer=0
     for i in range(n):
-        register.append(1)
+        total=0
+        # total+=myArr[i]
+        # if i>=0:
+        #     total+=1
+        if i>=1:
+            total+=myArr[i-1]
+        if i>=2:
+            total+=myArr[i-2]
+        if i>=3:
+            total+=myArr[i-3]
+        myArr.append(total)
+    for num in myArr:
+        answer+=num
+    return answer
 
-def permintations():
-    pass
 
 
 if __name__ == "__main__":
@@ -20,18 +36,55 @@ if __name__ == "__main__":
     #draw out what the answer should be
     """
     total permintations
+    1
+
+    1,1
+    2
+
+    111
+    12
+    21
+
+    3
+
+    1111
+    112
+    121
+    211
+    22
+
+    13
+    31
+
+    5
     1,1,1,1,1
     1,1,1,2
     1,1,2,1
     1,2,1,1
     2,1,1,1
+    1,2,2
     2,1,2
     2,2,1
+
     1,1,3
     1,3,1
     3,1,1
     2,3
     3,2
+
+    6
+    1,1,1,1,1,1
+    1,1,1,1,2
+    1,1,1,2,1
+    1,1,2,1,1
+    1,2,1,1,1
+    2,1,1,1,1
+    1,1,2,2
+    1,2,1,2
+    2,1,1,2
+    2,1,2,1
+    2,2,1,1
+    2,2,2
     """
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
